79ec927e81a940d0fa340c75809ee0741d048460
Luuk Vogel <luukbird@gmail.com>
2022-04-25 22:51:57 +0200

    User can now be selected by id and can be editted. Also the user can be removed by id.

--- index.js
+++ index.js
@@ -22,41 +22,43 @@ app.get("/", (req, res) => {
     });
 });
 
-// Adds a new user in the database
-app.post("/api/user", (req, res) => {
-    let user = req.body;
+app.route("/api/user")
+    // Adds a new user in the database
+    .post((req, res) => {
+        let user = req.body;
 
-    if (isEmailUnique(user.emailAddress)) {
-        user_id++;
-        user = {
-            id: user_id,
-            firstName: user.firstName,
-            lastName: user.lastName,
-            street: user.street,
-            city: user.city,
-            emailAddress: user.emailAddress,
-            phoneNumber: user.phoneNumber,
-            password: user.password,
-            roles: user.roles,
-        };
-        database.push(user);
-        res.status(201).json({
-            status: 201,
+        if (isEmailUnique(user.emailAddress)) {
+            user_id++;
+            user = {
+                id: user_id,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                street: user.street,
+                city: user.city,
+                emailAddress: user.emailAddress,
+                phoneNumber: user.phoneNumber,
+                password: user.password,
+                roles: user.roles,
+            };
+            database.push(user);
+            res.status(201).json({
+                status: 201,
+                result: database,
+            });
+        } else {
+            res.status(409).json({
+                status: 409,
+                result: "Email is already in use",
+            });
+        }
+    })
+    // Gets all users from database
+    .get((req, res) => {
+        res.status(200).json({
+            status: 200,
             result: database,
         });
-    } else {
-        res.status(409).json({
-            status: 409,
-            result: "Email is already in use",
-        });
-    }
-    // Gets all users from database
-}).get((req, res) => {
-    res.status(200).json({
-        status: 200,
-        result: database,
     });
-});
 
 // Gets the profile of the requested user
 app.get("/api/user/profile", (req, res) => {
@@ -66,10 +68,84 @@ app.get("/api/user/profile", (req, res) => {
     });
 });
 
+// Gets the user by id
+app.route("/api/user/:id")
+    .get((req, res) => {
+        const userId = req.params.id;
+        let user = database.find((item) => item.id == userId);
+
+        if (user) {
+            res.status(200).json({
+                status: 200,
+                result: user,
+            });
+        } else {
+            res.status(404).json({
+                status: "User does not exist",
+            });
+        }
+    })
+    // Updates the user
+    .put((req, res) => {
+        let newUserInfo = req.body;
+        const userId = req.params.id;
+        let userIndex = database.findIndex((obj) => obj.id == userId);
+
+        if (userIndex > -1) {
+            if (Array.isArray(newUserInfo.roles)) {
+                database[userIndex] = {
+                    id: parseInt(userId),
+                    firstName: newUserInfo.firstName,
+                    lastName: newUserInfo.lastName,
+                    street: newUserInfo.street,
+                    city: newUserInfo.city,
+                    phoneNumber: newUserInfo.phoneNumber,
+                    password: newUserInfo.password,
+                    emailAdress: newUserInfo.emailAdress,
+                    roles: newUserInfo.roles,
+                };
+
+                res.status(200).json({
+                    status: 200,
+                    result: database[userIndex],
+                });
+            } else {
+                res.status(400).json({
+                    status: 400,
+                    result: "Roles must be an array",
+                });
+            }
+        } else {
+            res.status(404).json({
+                status: 404,
+                result: "User not found",
+            });
+        }
+    })
+    // Deletes the user from the database
+    .delete((req, res) => {
+        const userId = req.params.id;
+        let userIndex = database.findIndex((obj) => obj.id == userId);
+
+        if (userIndex > -1) {
+            database.splice(userIndex, 1);
+
+            res.status(202).json({
+                status: 202,
+                result: "User is successfully deleted",
+            });
+        } else {
+            res.status(404).json({
+                status: 404,
+                result: "User does not exist",
+            });
+        }
+    });
+
 app.all("*", (req, res) => {
     res.status(401).json({
         status: 401,
-        result: "End-point not found",
+        result: "Endpoint not found",
     });
 });
 
