d45dbbe29ae73c8abf41fd4eb4e30bcf39d067ad
Luuk Vogel <luukbird@gmail.com>
2022-05-11 23:18:08 +0200

    Made tests for adding user

--- database/dbconnection.js
+++ database/dbconnection.js
@@ -1,16 +1,19 @@
 require('dotenv').config();
 const mysql = require('mysql');
 const pool = mysql.createPool({
-    connectionLimit: 10,
+    connectionLimit: 10,  
+    waitForConnections: true,
+    queueLimit: 0,
+    multipleStatements: true,
     host : process.env.DB_HOST,
     user : process.env.DB_USER,
     port : process.env.DB_PORT,
     password : process.env.DB_PASSWORD,
     database : process.env.DB_DATABASE,
 });
- 
-module.exports = pool;
 
 pool.on('acquire', function (connection) {
   console.log('Connection %d acquired', connection.threadId);
 });
+
+module.exports = pool;
